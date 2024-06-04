13908f573dd158f97530ee73d8c8c6d2f800a3b7
Sergiy Savostin <sergiy.savostin@totalperformancedata.com>
2024-06-04 13:37:29 +0100

    vendor key

--- api/php/streaming_request.php
+++ api/php/streaming_request.php
@@ -1,10 +1,14 @@
 <?php
-   
-    include_once('creds.php'); // (added to gitignore - see creds_example.php)
 
-    $_SERVER['REMOTE_ADDR'] = $_SERVER['REMOTE_ADDR'] == "::1" ? $local_ip : $_SERVER['REMOTE_ADDR'];
+include_once ('creds.php'); // (added to gitignore - see creds_example.php)
+
+$_SERVER['REMOTE_ADDR'] = $_SERVER['REMOTE_ADDR'] == "::1" ? $local_ip : $_SERVER['REMOTE_ADDR'];
 header('Content-type: application/json');
-    echo 
-    (isset($k) && isset($vk) && isset($_GET['sc'])) 
-    ? file_get_contents($target . "/json-rpc/v2/vendors/streaming_request/?mode={$_GET['mode']}&sc={$_GET['sc']}&ip={$_SERVER['REMOTE_ADDR']}&k={$k}")
-    : [];
+if (isset($k) && isset($vk) && isset($_GET['sc'])) {
+    $data = file_get_contents($target . "/json-rpc/v2/vendors/streaming_request/?mode={$_GET['mode']}&sc={$_GET['sc']}&ip={$_SERVER['REMOTE_ADDR']}&k={$k}");
+    $d = json_decode($data, true);
+    $d['vendorKey'] = "15b27927b4ca444840a94d75b4e253ab"; // TODO
+    echo json_encode($d);
+} else {
+    echo '[]';
+}
