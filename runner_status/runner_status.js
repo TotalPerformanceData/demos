ecf2d430c69741fd05234e8aedf785a5d8fbdd83
Sergiy Savostin <sergiy.savostin@totalperformancedata.com>
2024-10-03 12:32:15 +0100

    Update runner_status.js

--- runner_status/runner_status.js
+++ runner_status/runner_status.js
@@ -1,7 +1,7 @@
 class RunnerStatus {
     static API_URL = "/demos/api/php";
     static SILKS_URL = 'https://d5r512lauia4d.cloudfront.net/';
-    static DEBUG = window.location.host.match(/stg/);
+    static DEBUG = false;
     static WS_URL = `wss://stream.tpd.zone/realtime_${RunnerStatus.DEBUG ? 'ws_dev' : '1.4'}`
     static VERSION = `2024.03.19${RunnerStatus.DEBUG ? '-dev' : ''}`;
 
