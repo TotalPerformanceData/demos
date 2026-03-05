519afc06e19af74835d5430c4769b1b918418b6c
Sergiy Savostin <sergiy.savostin@totalperformancedata.com>
2026-03-05 17:28:37 +0000

    Update WS_URL to use stable version

--- runner_status/runner_status.js
+++ runner_status/runner_status.js
@@ -2,7 +2,7 @@ class RunnerStatus {
     static API_URL = "/demos/api/php";
     static SILKS_URL = 'https://d5r512lauia4d.cloudfront.net/';
     static DEBUG = false;
-    static WS_URL = `wss://stream.tpd.zone/realtime_${RunnerStatus.DEBUG ? 'ws_dev' : '1.4'}`
+    static WS_URL = `wss://stream.tpd.zone/realtime_1.4`
     static VERSION = `2024.03.19${RunnerStatus.DEBUG ? '-dev' : ''}`;
 
     static Type = {
